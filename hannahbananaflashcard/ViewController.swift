a57bc2e01335295bf26b83a108a1a25e4400b218
iD Student <idstudent@iDs-iMac-4.local>
2017-07-19 10:37:55 -0700

    heyyyyy

--- hannahbananaflashcard/ViewController.swift
+++ hannahbananaflashcard/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func button1(_ sender: Any) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
